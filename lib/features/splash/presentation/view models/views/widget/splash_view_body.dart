cea9506303955aff67e7a5b4b9f396d5b51e2a6e
ToDo_App <somayasaeid75@gmail.com>
2024-11-23 22:31:59 +0200

    create splash view body. splash view ui is done. change theme. add text to splash view body.

--- lib/features/splash/presentation/view models/views/widget/splash_view_body.dart
+++ lib/features/splash/presentation/view models/views/widget/splash_view_body.dart
@@ -1,10 +1,34 @@
+import 'package:bookly_app1/features/splash/presentation/view%20models/views/widget/slidingText.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../../../core/utils/assets.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 7), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,16 +36,25 @@ class SplashViewBody extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Image.asset(AssetsData.logo ,),
-        const SizedBox(
-          height: 5,
+        Image.asset(
+          AssetsData.logo,
         ),
-        Text(
-          'read free books',
-          textAlign: TextAlign.center,
-          style: Theme.of(context).textTheme.titleSmall,
-        )
+        const SizedBox(),
+        SlidingText(slidingAnimation: slidingAnimation),
       ],
     );
+  }
+
+  void initSlidingAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
+            .animate(animationController);
+
+    animationController.forward();
   }
 }
