55d857e439bf02b4a8729b234b9f8e2cce17b14d
ToDo_App <somayasaeid75@gmail.com>
2024-12-02 00:07:04 +0200

    add custom scroll view

--- lib/features/home/presentation/views/widget/book_details_view_body.dart
+++ lib/features/home/presentation/views/widget/book_details_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_app1/features/home/presentation/views/widget/book_rating.dart';
 import 'package:bookly_app1/features/home/presentation/views/widget/custom_book_item.dart';
+import 'package:bookly_app1/features/home/presentation/views/widget/similar_books_list_view.dart';
 import 'package:flutter/material.dart';
 import '../../../../../core/utils/constants.dart';
 import 'book_action.dart';
@@ -10,40 +11,63 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 30.0,
-        vertical: 30,
-      ),
-      child: Column(
-        children: [
-          const CustomBookDetailsAppBar(),
-          Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: Constant.getWidth(context) * .17,
-              vertical: Constant.getHeight(context) * .03,
+    return CustomScrollView(
+
+      slivers: [
+
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 30.0,
+              vertical: 30,
             ),
-            child: const CustomBookItem(),
+            child: Column(
+              children: [
+                const CustomBookDetailsAppBar(),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: Constant.getWidth(context) * .2,
+                    vertical: Constant.getHeight(context) * .03,
+                  ),
+                  child: const CustomBookItem(),
+                ),
+                Text(
+                  'The Jungle Book',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                const SizedBox(
+                  height: 6,
+                ),
+                Text(
+                  'Rudyard Kipling',
+                  style: Theme.of(context).textTheme.bodyMedium,
+                ),
+                const SizedBox(
+                  height: 6,
+                ),
+                const BookRating(),
+                const SizedBox(height: 30),
+                const BookAction(),
+                const Expanded(
+                  child: SizedBox(
+                    height: 50,
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'You can also like',
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+                ),
+                const SizedBox(height: 16),
+                const SimilarBooksListView(),
+              ],
+            ),
           ),
-          Text(
-            'The Jungle Book',
-            style: Theme.of(context).textTheme.titleLarge,
-          ),
-          const SizedBox(
-            height: 6,
-          ),
-          Text(
-            'Rudyard Kipling',
-            style: Theme.of(context).textTheme.bodyMedium,
-          ),
-          const SizedBox(
-            height: 6,
-          ),
-          const BookRating(),
-          const SizedBox(height: 30),
-          const BookAction(),
-        ],
-      ),
+        )
+      ],
     );
   }
 }
---
+++ lib/features/home/presentation/views/widget/similar_books_list_view.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+
+import 'custom_book_item.dart';
+
+class SimilarBooksListView extends StatelessWidget {
+  const SimilarBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .18,
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8.0),
+              child: CustomBookItem(),
+            );
+          }),
+    );
+      }
+}
