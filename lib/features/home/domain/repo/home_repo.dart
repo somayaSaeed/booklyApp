800bd9f9259ac9c3afe940ecdd955bd6a41fe32e
ToDo_App <somayasaeid75@gmail.com>
2024-12-03 16:57:31 +0200

    add either class.

--- lib/features/home/domain/repo/home_repo.dart
+++ lib/features/home/domain/repo/home_repo.dart
@@ -1,7 +1,9 @@
 import 'package:bookly_app1/features/home/domain/entities/book_entity.dart';
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/failure.dart';
 
 abstract class HomeRepo {
-  Future<List<BookEntity>> fetchFeaturedBook();
-  Future<List<BookEntity>> fetchNewsBooks();
-
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBook();
+  Future<Either<Failure, List<BookEntity>>> fetchNewsBooks();
 }
